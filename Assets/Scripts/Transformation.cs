50c57e62fa2c96acdcd8d03cfaa83d830a29eb2b
emr <renquin@awtce.be>
2018-07-05 13:28:06 +0200

    Transformations de base (section 2 et 3)

---
+++ Assets/Scripts/Transformation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Security.Cryptography.X509Certificates;
+
+public abstract class Transformation : MonoBehaviour {
+
+	public abstract Vector3 Apply (Vector3 point);
+
+}
